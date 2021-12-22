b886b231a9eb6f64471d05aa28f2db3d98b2a190
Shane Holloway <shane.holloway@ieee.org>
2021-12-22 10:59:09 -0700

    Finished node-arch example with Workers and Subprocesses

---
+++ examples/node-arch/fork.mjs
@@ -0,0 +1,21 @@
+import MsgFabric from 'msg-fabric-core/esm/mf-json-node.js'
+
+console.log('Subporcess started')
+
+export const mf_hub = MsgFabric.create()
+export default mf_hub
+
+// use process to access spawning IPC channel -- see https://nodejs.org/dist/latest-v17.x/docs/api/child_process.html#subprocesssendmessage-sendhandle-options-callback
+export const mf_chan_parent =
+  mf_hub.direct.connectMsgPort(process, {
+    debug(msg, fn) { console.info('MSG:', msg, fn) },
+  })
+
+mf_chan_parent.then(async chan => {
+  console.log('Subporcess connecting to main.mjs:', chan)
+  let pi = await chan.peer_info
+  if (pi)
+    console.log('Subporcess connected to main.mjs:', chan, pi)
+  else 
+    console.log('Subporcess failed to connect to main.mjs:', chan, pi)
+})
---
+++ examples/node-arch/worker.mjs
@@ -0,0 +1,20 @@
+import { parentPort } from 'node:worker_threads'
+import MsgFabric from 'msg-fabric-core/esm/mf-json-node.js'
+
+console.log('Worker Thread started')
+
+export const mf_hub = MsgFabric.create()
+export default mf_hub
+
+
+export const mf_chan_parent =
+  mf_hub.direct.connectMsgPort(parentPort)
+
+mf_chan_parent.then(async chan => {
+  console.log('Worker Thread connecting to main.mjs:', chan)
+  let pi = await chan.peer_info
+  if (pi)
+    console.log('Worker Thread connected to main.mjs:', chan, pi)
+  else 
+    console.log('Worker Thread failed to connect to main.mjs:', chan, pi)
+})
