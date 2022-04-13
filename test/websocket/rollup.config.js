90ca51efe630e7e07843f78dc3b80ba421d73e6b
Shane Holloway <shane.holloway@ieee.org>
2022-04-13 08:22:35 -0600

    Updated dependencies

--- test/websocket/rollup.config.js
+++ test/websocket/rollup.config.js
@@ -1,6 +1,7 @@
-import rpi_resolve from 'rollup-plugin-node-resolve'
-import rpi_commonjs from 'rollup-plugin-commonjs'
-import rpi_jsy from 'rollup-plugin-jsy-lite'
+import {builtinModules} from 'module'
+import rpi_resolve from '@rollup/plugin-node-resolve'
+import rpi_commonjs from '@rollup/plugin-commonjs'
+import rpi_jsy from 'rollup-plugin-jsy'
 
 const sourcemap = 'inline'
 const test_plugins = [
@@ -8,18 +9,19 @@ const test_plugins = [
   rpi_commonjs({ include: 'node_modules/**'}),
 ]
 
+const external = id => /^\w+:/.test(id) || builtinModules.includes(id)
 const plugins_nodejs = [ rpi_jsy({defines: {PLAT_NODEJS: true}}) ].concat(test_plugins)
 const plugins_web = [ rpi_jsy({defines: {PLAT_WEB: true}}) ].concat(test_plugins)
 
 
 export default [
-  { input: '../browser.hub.js', plugins: plugins_web,
+  { input: '../browser.hub.js', plugins: plugins_web, external,
     output: { file: `umd/browser.js`, format: 'umd', name: 'msg-fabric-core', sourcemap } },
 
-  { input: 'websocket-WS.jsy', plugins: plugins_nodejs,
+  { input: 'websocket-WS.jsy', plugins: plugins_nodejs, external,
     output: { file: `cjs/websocket-WS.js`, format: 'cjs', sourcemap } },
 
-  { input: 'websocket-FAYE.jsy', plugins: plugins_nodejs,
+  { input: 'websocket-FAYE.jsy', plugins: plugins_nodejs, external,
     output: { file: `cjs/websocket-FAYE.js`, format: 'cjs', sourcemap } },
 
 ].filter(Boolean)
